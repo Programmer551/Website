3448a8acf9c1876806a054825d86abd83dfe10dc
Programmer551 <m.duraid551@gmail.com>
2023-11-08 16:20:01 +0500

    Update

--- server.js
+++ server.js
@@ -2,16 +2,16 @@ const express = require("express");
 const app = express();
 const router = require("./routes/routes");
 const connectDB = require("./db/db");
-const port = 3000;
-const hostname = "0.0.0.0";
+const port = 5000;
+// const hostname = "0.0.0.0";
 require("dotenv").config();
 app.use("/", express.static("public"));
 app.use(express.json());
 app.use("/", router);
 try {
   connectDB(process.env.MONGO_URI);
-  app.listen(port, hostname, () => {
-    console.log(`Server is listening http://${hostname}:${port}`);
+  app.listen(port, () => {
+    console.log(`Server is listening http://localhost:${port}`);
     // console.log(`Server is listening http://${hostname}:${port}`);
   });
 } catch (error) {
